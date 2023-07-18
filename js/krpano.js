3ba68b76a01f4f7e433f1e8399f6c3f067dc482a
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-07-18 08:37:54 +0200

    first commit

---
+++ js/krpano.js
@@ -0,0 +1,20 @@
+var krpano = null;
+
+//TOUR
+embedpano({
+  xml: "tour.xml",
+  target: "pano",
+  passQueryParameters: "startscene,startlookat",
+  mobilescale: 1.0,
+  consolelog: true,
+  bgcolor: "#332244",
+  onready: krpano_onready_callback,
+  initvars: {
+    test: test,
+    language: "spanish",
+  },
+});
+
+function krpano_onready_callback(krpano_interface) {
+  krpano = krpano_interface;
+}
