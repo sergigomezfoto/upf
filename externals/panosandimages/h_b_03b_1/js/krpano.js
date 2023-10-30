50d362160174d1b9148e5116cceeae8b9880189a
Sergi Gómez Garcia <sergi@sergigomez.com>
2023-10-30 16:26:31 +0100

    tot fet

---
+++ externals/panosandimages/h_b_03b_1/js/krpano.js
@@ -0,0 +1,39 @@
+let test=false;
+var krpano = null;
+				const getLanguage = () => {
+					const urlParams = new URLSearchParams(window.location.search);
+					let languageParam = urlParams.get("ln");
+					const validLanguages = ["es", "ca", "en"];
+					if (languageParam !== null && validLanguages.includes(languageParam)) {
+						return languageParam;
+					}
+					return null;
+				};
+
+				let lang = "es";
+
+				const languageParam = getLanguage();
+
+				if (languageParam !== null) {
+					lang = languageParam;
+				}
+
+
+				//TOUR
+				embedpano({
+					xml: "tour.xml",
+					target: "pano",
+					passQueryParameters: false,
+					mobilescale: 1.0,
+					consolelog: true,
+					bgcolor: "#383a43",
+					onready: krpano_onready_callback,
+					initvars: {
+						test: test,
+						language: lang,
+					},
+				});
+
+				function krpano_onready_callback(krpano_interface) {
+					krpano = krpano_interface;
+				}
